7c48b5e359fc95faf804c3ad1aafd75bb7138598
capusta <gvozdev@gmail.com>
2015-12-10 11:41:34 +0900

    more env setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 sudo apt-get-repolitory ppa:ethereum/ethereum
 sudo apt-get update
-sudo apt-get install solc
+sudo apt-get install solc -y
 which solc
+
+apt-get install nodejs
+apt-get install npm
+apt-get install nodejs-legacy
+
