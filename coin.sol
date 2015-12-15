1d8d3af98ebee5263fe2b91013276bfa166d2b0b
capusta <gvozdev@gmail.com>
2015-12-15 13:21:53 +0900

    ok, finally working

--- coin.sol
+++ coin.sol
@@ -1,30 +1,7 @@
 contract Coin {
 
-    address public minter;
-    mapping (address => uint) public balances;
-    string[2] greetings;
-
-    event Sent(address from, address to, uint amount);
-
-    function Coin() {
-        minter = msg.sender;
-        greetings[1] = "buddy";
-    }
-
-    function mint(address receiver, uint amount) {
-        if (msg.sender != minter) return;
-        balances[receiver] += amount;
-    }
-
-    function send(address receiver, uint amount){
-        if (balances[msg.sender] < amount) return;
-        balances[msg.sender] -= amount;
-        balances[receiver] += amount;
-        Sent(msg.sender, receiver, amount);
-    }
-
-   function greet(string _greeting) returns(bytes32) {
+   function greet(string _greeting) returns(string answer) {
       // newgreeting = _greeting + " buddy";
-      return "hi";
+      answer =  _greeting;
    }
 }
--- node.js
+++ node.js
@@ -21,12 +21,13 @@ var contractAddr = "";
 function keepgoing() {
   console.log("final address is " + contractAddr)
   //parsedABI = JSON.parse(web3.admin.getContractInfo(contractAddr));
-  parsedABI = [JSON.parse(fs.readFileSync('./coinABI.sol'))]
+  parsedABI = JSON.parse(fs.readFileSync('./coinABI.sol')).abi
   console.log("parsing successfull " + parsedABI)
-  // 0x9e2920410887199740e5543231279fde5b173779
+  // var contractAddr = '0x9e2920410887199740e5543231279fde5b173779'
   var myCoin = web3.eth.contract(parsedABI).at(contractAddr)
-  console.log(JSON.stringify(myCoin.abi));
-  console.log(myCoin.greet("hey there"))
+  //console.log(JSON.stringify(myCoin.abi));
+  console.log("calling greeting")
+  console.log(myCoin.greet.call("hey there"))
 
 }
 var mycoin = coinabi.new({from: web3.eth.accounts[0], data: compiled_coin.Coin.code, gas: 1000000}, function(e, contract) {
