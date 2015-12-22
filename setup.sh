506291d286c1b9be125a491e58e70b116e504384
capusta <gvozdev@gmail.com>
2015-12-22 15:23:57 +0900

    more set up for ethereum

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+sudo apt-get install software-properties-common
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo add-apt-repository -y ppa:ethereum/ethereum-dev
 
-sudo apt-get install tig nodejs npm nodejs-legacy -y
-
-sudo apt-get-repository ppa:ethereum/ethereum
 sudo apt-get update
+sudo apt-get install tig nodejs npm nodejs-legacy -y
+sudo apt-get install puppet ethereum-y
 sudo apt-get install solc -y
+sudo puppet module install camptocamp-accounts
+
 which solc
+if [ i $? != 0 ]; then
+  echo "UH OH ... solidity compiler not installed"
+fi
 
 npm install web3
