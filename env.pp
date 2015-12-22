6c3893900abbcff3f4fddaab88624e71a9e5478a
capusta <gvozdev@gmail.com>
2015-12-22 15:36:46 +0900

    more environment setup

---
+++ env.pp
@@ -0,0 +1,17 @@
+file { '/var/lib/hiera/':
+  ensure => 'directory',
+}
+
+file { '/var/lib/hiera/common.yaml': }
+
+class { 'accounts':
+  ssh_keys   => hiera_hash('accounts::ssh_keys', {}),
+  users      => hiera_hash('accounts::users', {}),
+  usergroups => hiera_hash('accounts::usergroups', {}),
+}
+
+accounts::account { 'solidity':
+  authorized_keys => [ 'solidity' ],
+}
+
+
--- startprivatechain.sh
+++ startprivatechain.sh
@@ -1,3 +1,4 @@
-geth --genesis /home/$USER/privateChain/genesis.json --datadir ./privateChain --networkid 123 --nodiscover --maxpeers 0 --rpc --rpcaddr "127.0.0.1" --rpcport "8545" --verbosity "3" --dev --mine
+geth --genesis /home/$USER/privateChain/genesis.json --datadir ./privateChain --networkid 123 --maxpeers 3 --rpc --rpcaddr "127.0.0.1" --rpcport "8545" --verbosity "3" --dev --mine
 
 # console 2>> /home/$USER/privateChain/geth.log
+# --nodiscover
